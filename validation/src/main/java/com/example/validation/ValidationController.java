73ac7daf408b231b6bdad6f5fb10aacf4af53d90
MarianyLopez <122067825+MarianyLopez@users.noreply.github.com>
2023-08-10 20:29:09 -0500

    dependency injections

--- validation/src/main/java/com/example/validation/ValidationController.java
+++ validation/src/main/java/com/example/validation/ValidationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ValidationController {
     private final ValidationService validationService;
 
+    @Autowired()
     public ValidationController(ValidationService validationService) {
         this.validationService = validationService;
     }
